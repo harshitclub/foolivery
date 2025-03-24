87e53406b5f241839a8023da4d30db58d9fbc2f9
harshitclub <harshitclub@gmail.com>
2025-03-24 10:37:08 +0530

    js

---
+++ script.js
@@ -0,0 +1,78 @@
+// JavaScript Variable
+/*
+there are 3 types of variable available in javascript
+1. var
+2. const
+3. let
+*/
+
+// var myName = "Harshit Kumar";
+
+// console.log(myName);
+
+// let myName = "Harshit Kumar";
+// console.log(myName);
+
+// myName = "Kumar Kumar";
+// console.log(myName);
+
+// console.log(myName);
+
+// const myName = "Harshit Kumar";
+
+// myName = "Kumar Kumar";
+
+// console.log(myName);
+
+// demo = "Harshit Kumar";
+// console.log(demo);
+
+// const mYname;
+// mYname = "random string";
+// console.log(mYname);
+
+/*
+there are two data types in javascript
+
+1. Primitive
+2. Non Primitive
+*/
+
+/*
+Primitive:
+
+String - "Harshit Kumar"
+Number - 0099
+Boolean - True/False
+Undefined 
+Null
+
+Non-Primitve:
+
+1. Array - [1,2,3,4,5]
+2. Object - {name: "Harshit", age: 23}
+*/
+
+// variables
+// data types
+// array in javascript
+// object in javascript
+
+// arithmetic operators: +,-,*,/,%,++,--
+// comparison operators: ==, ===, <,>,<=,>=, !==
+
+const num1 = 49;
+const num2 = 40;
+
+console.log("Equals to result: ", num1 == num2);
+console.log("Less than result: ", num1 < num2);
+console.log("Greater than result: ", num1 > num2);
+console.log("Less than equals to: ", num1 <= num2);
+console.log("Greater than equals to: ", num1 >= num2);
+console.log("Not equals to: ", num1 !== num2);
+
+const myval = 90;
+const myval2 = 99;
+
+console.log(myval < myval2 && num1 > num2);
+console.log(myval > myval2 || num1 < num2);
