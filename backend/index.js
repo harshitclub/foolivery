6106983e05270d9f34b73b2080fc48ed01637a5b
harshitclub <harshitclub@gmail.com>
2025-04-30 09:37:21 +0530

    backend express

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,11 @@
-const path = require("path");
-const http = require("http");
-const fs = require("fs");
-// const hostname = "127.0.0.1";
+const express = require("express");
+const dotenv = require("dotenv");
 
-const indexFile = path.join(__dirname, "./public/index.html");
+const app = express();
+dotenv.config();
 
-const server = http.createServer((req, res) => {
-  fs.readFile(indexFile, (err, data) => {
-    if (err) {
-      console.error("Error reading HTML file:", err);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-});
+const PORT = process.env.PORT || 5000;
 
-server.listen(3000, () => {
-  console.log("server started");
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
 });
