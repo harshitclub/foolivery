33988467177b66e5371afbe59b49f1d36090ef39
harshitclub <harshitclub@gmail.com>
2025-04-30 10:05:12 +0530

    aa

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,22 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res
+    .json({
+      message: "hello is from backend",
+    })
+    .status(200);
+});
+
+app.get("/about", (req, res) => {
+  res.send("this is the about page").status(200);
+});
+
+app.get("/contact", (req, res) => {
+  res.json({ message: "this is the contact page" }).status(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
