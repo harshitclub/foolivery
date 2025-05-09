222c64bad4238db0ace940774cee1e0a42db81f5
harshitclub <harshitclub@gmail.com>
2025-05-09 10:38:49 +0530

    h

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/global/navbar/Navbar";
 import Footer from "./components/global/footer/Footer";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import axios from "axios";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+  const getProfile = async () => {
+    const profile = await axios.get("http://localhost:5000/profile", {
+      withCredentials: true,
+    });
+    setUser(profile.data.data);
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
+  return (
+    <main>
+      <h1>{user ? user.fullName : null}</h1>
+    </main>
+  );
 };
 
 export default Profile;
